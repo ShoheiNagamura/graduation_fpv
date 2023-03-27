714aff4d72c3fe567053e34f722d71695aca5adf
shouhei <shohei.nagamura@gmail.com>
2023-03-28 02:16:03 +0900

    userテーブルを定義し、ファクトリ、シードを定義

--- database/migrations/2023_03_23_180709_create_pilots_table.php
+++ database/migrations/2023_03_23_180709_create_pilots_table.php
@@ -17,6 +17,11 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('user_image')->nullable(); //ユーザー画像（追記）
+            $table->integer('age')->nullable(); //年齢（追記）
+            $table->string('work_area', 255)->nullable(); //都道府県（追記）
+            $table->string('message_pr', 500)->nullable(); //PRメッセージ（追記）
+            $table->string('achievement', 500)->nullable(); //実績など（追記）
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
