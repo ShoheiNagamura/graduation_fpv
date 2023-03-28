f200f580f8f5d28d563edc0fdb49f630c04f2c41
shouhei <shohei.nagamura@gmail.com>
2023-03-28 11:47:11 +0900

    シーダーファイルを修正

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use app\Models\Portfolio;
 use app\Models\ReferencePlan;
+use App\Models\User;
 use App\Models\Pilot;
 use App\Models\ShootingPlan;
 
@@ -18,27 +19,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //Userテーブル用
-        \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-
-        //Pilotテーブル用
-        // \App\Models\Pilot::factory(10)->create();
+        //Userテーブル用 10人作成
+        $users = User::factory(10)->create();
 
 
-        //Portfolioテーブル用
-        // \App\Models\Portfolio::factory(10)->create();
+        //Pilotテーブル用 10人作成
+        $pilots = Pilot::factory(10)->create();
 
 
-        //PilotとPortfolioを紐づけてデータを作成
-        Pilot::factory()->count(10)->create()->each(function ($pilot) {
+        //Portfolio用ダミーデータ
+        foreach ($pilots as $pilot) {
             $pilot->portfolios()->saveMany(Portfolio::factory()->count(3)->create(['pilot_id' => $pilot->id]));
+        };
+
+
+        //reference_plans用ダミーデータ
+        foreach ($pilots as $pilot) {
             $pilot->reference_plans()->saveMany(ReferencePlan::factory()->count(2)->create(['pilot_id' => $pilot->id]));
+        };
+
+        //shooting_plans用ダミーデータ
+        foreach ($pilots as $pilot) {
             $pilot->shooting_plans()->saveMany(ShootingPlan::factory()->count(1)->create(['pilot_id' => $pilot->id]));
-        });
+        };
     }
 }
