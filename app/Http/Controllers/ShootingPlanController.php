d4bc04983ae870dd4a36c2d2fc3636d71c0b19a6
shouhei <shohei.nagamura@gmail.com>
2023-03-31 20:41:28 +0900

    共通エラーパーツ作成

--- app/Http/Controllers/ShootingPlanController.php
+++ app/Http/Controllers/ShootingPlanController.php
@@ -7,76 +7,76 @@ use Illuminate\Http\Request;
 class ShootingPlanController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
      * @return \Illuminate\Http\Response
      */
+
+    // 登録済み一覧画面 ----------------------------
     public function index()
     {
         //
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
      * @return \Illuminate\Http\Response
      */
+
+    // プラン登録画面 ---------------------------------
     public function create()
     {
         //
     }
 
     /**
-     * Store a newly created resource in storage.
-     *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
+
+    //-----------------------------------------------
     public function store(Request $request)
     {
         //
     }
 
     /**
-     * Display the specified resource.
-     *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
+
+    //  プラン詳細画面 ------------------------------------
     public function show($id)
     {
         //
     }
 
     /**
-     * Show the form for editing the specified resource.
-     *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
+
+    // プラン編集画面 -------------------------------------
     public function edit($id)
     {
         //
     }
 
     /**
-     * Update the specified resource in storage.
-     *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
+
+    // ------------------------------------------------
     public function update(Request $request, $id)
     {
         //
     }
 
     /**
-     * Remove the specified resource from storage.
-     *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
+
+    //-----------------------------------------------------
     public function destroy($id)
     {
         //
