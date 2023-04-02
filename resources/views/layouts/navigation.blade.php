294c9bad0a4ebb8143a7b5f3f4f54249cf93440a
shouhei <shohei.nagamura@gmail.com>
2023-04-03 02:12:45 +0900

    パイロット認証でパイロットリストが表示されるように実装

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,6 +16,13 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+
+
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('pilot_list.index')" :active="request()->routeIs('pilot_list.index')">
+                        {{ __('パイロットリスト') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
