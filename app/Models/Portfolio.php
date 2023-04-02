bc40f910127a3926e00474bc2cb8db1805c32b3d
shouhei <shohei.nagamura@gmail.com>
2023-04-02 13:30:05 +0900

    ポートフォリオを登録、更新、読み込み、削除を実装

--- app/Models/Portfolio.php
+++ app/Models/Portfolio.php
@@ -9,15 +9,22 @@ class Portfolio extends Model
 {
     use HasFactory;
 
+    // 変更されない
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at',
+    ];
+
+    // 変更可能
+    protected $fillable = [
+        'pilot_id',
+        'portfolio_url',
+    ];
+
     // 多対1
     public function pilot()
     {
         return $this->belongsTo(Pilot::class);
     }
-
-
-    // 変更可能
-    protected $fillable = [
-        'portfolio_url',
-    ];
 }
--- resources/views/layouts/pilot_navigation.blade.php
+++ resources/views/layouts/pilot_navigation.blade.php
@@ -28,6 +28,19 @@
                         {{ __('プラン作成') }}
                     </x-nav-link>
                 </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('pilot.portfolio.index')" :active="request()->routeIs('pilot.portfolio.index')">
+                        {{ __('ポートフォリオ') }}
+                    </x-nav-link>
+                </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('pilot.portfolio.create')" :active="request()->routeIs('pilot.portfolio.create')">
+                        {{ __('ポートフォリオ作成') }}
+                    </x-nav-link>
+                </div>
+
             </div>
 
             <!-- Settings Dropdown -->
