d136ec31cb151a991417268b51bc21978c7a4f38
shouhei <shohei.nagamura@gmail.com>
2023-04-04 22:06:32 +0900

    CSSを修正

--- resources/views/pilot_list/show.blade.php
+++ resources/views/pilot_list/show.blade.php
@@ -14,7 +14,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <div class="mb-6">
-                        <div class="flex flex-col mb-4">
+                        <div class="mb-4">
                             <h2 class="mb-4 uppercase font-bold text-center text-grey-darkest">パイロット詳細</h2>
                             <div>
                                 <h3 class="text-left font-bold text-lg text-grey-dark">お名前：{{$pilot->name}}({{$pilot->age}})</h3>
@@ -86,7 +86,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <div class="mb-6">
-                        <div class="flex flex-col mb-4">
+                        <div class="mb-4">
                             <h2 class="mb-4 uppercase font-bold text-center text-grey-darkest">パイロット詳細</h2>
                             <div>
                                 <h3 class="text-left font-bold text-lg text-grey-dark">お名前：{{$pilot->name}}({{$pilot->age}})</h3>
@@ -146,3 +146,8 @@
 </x-app-layout>
 
 @endif
+
+
+<a href="{{ route('pilot_list.show',$pilot->id) }}" class="block text-center w-full py-3 mt-6 font-medium tracking-widest text-white uppercase bg-black shadow-lg focus:outline-none hover:bg-gray-900 hover:shadow-none">
+    申し込み
+</a>
