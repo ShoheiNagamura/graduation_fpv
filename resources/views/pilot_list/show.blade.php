553e23c0ac988e000f75d3b9c9085724ba9bfcae
shouhei <shohei.nagamura@gmail.com>
2023-04-04 22:55:50 +0900

    レイアウト修正

--- resources/views/pilot_list/show.blade.php
+++ resources/views/pilot_list/show.blade.php
@@ -28,11 +28,11 @@
                             </div>
                         </div>
 
-                        <h2 class="mb-4 uppercase font-bold text-lg text-grey-darkest">プラン一覧</h2>
+                        <h2 class="mb-4 uppercase font-bold text-center text-lg text-grey-darkest">プラン一覧</h2>
 
                         <div class="flex">
                             @foreach ($pilot->pilotShootingPlans as $ShootingPlans)
-                            <div class="flex-1 hover:bg-grey-lighter">
+                            <div class="flex-1 border hover:bg-grey-lighter">
                                 <div class="py-4 px-6 border-b border-grey-light">
                                     <div class="">
                                         <div>
@@ -100,34 +100,38 @@
                             </div>
                         </div>
 
-                        <h2 class="mb-4 uppercase font-bold text-lg text-grey-darkest">プラン一覧</h2>
+                        <h2 class="mb-4 uppercase font-bold text-center text-lg text-grey-darkest">プラン一覧</h2>
 
                         <div class="flex">
                             @foreach ($pilot->pilotShootingPlans as $ShootingPlans)
-                            <div class="flex-1 hover:bg-grey-lighter">
+                            <div class="flex-1 border hover:bg-grey-lighter">
                                 <div class="py-4 px-6 border-b border-grey-light">
-                                    <div class="">
+                                    <div class="flex">
                                         <div>
-                                            <h4 class="text-left font-bold text-lg text-grey-dark">ID : {{$ShootingPlans->id}}</h4>
+                                            <div>
+                                                <h4 class="text-left font-bold text-lg text-grey-dark">ID : {{$ShootingPlans->id}}</h4>
+                                            </div>
+                                            <div>
+                                                <h3 class="text-left font-bold text-lg text-grey-dark">プラン名 : {{$ShootingPlans->plan_name}}</h3>
+                                            </div>
+                                            <div>
+                                                <p>報酬 : {{number_format($ShootingPlans->plan_fee)}}</p>
+                                            </div>
+                                            <div>
+                                                <p>申込日 : {{$ShootingPlans->application_date}}</p>
+                                            </div>
+                                            <div>
+                                                <p>撮影日 : {{$ShootingPlans->shooting_date}}</p>
+                                            </div>
+                                            <div>
+                                                <p>納品日 : {{$ShootingPlans->delivery_date}}</p>
+                                            </div>
+                                            <div class="w-30 text-center">
+                                                <a href="{{ route('pilot_list.show',$pilot->id) }}" class="block text-center w-full py-3 mt-6 font-medium tracking-widest text-white uppercase bg-black shadow-lg focus:outline-none hover:bg-gray-900 hover:shadow-none">
+                                                    申し込み
+                                                </a>
+                                            </div>
                                         </div>
-                                        <div>
-                                            <h3 class="text-left font-bold text-lg text-grey-dark">プラン名 : {{$ShootingPlans->plan_name}}</h3>
-                                        </div>
-                                        <div>
-                                            <p>報酬 : {{number_format($ShootingPlans->plan_fee)}}</p>
-                                        </div>
-                                        <div>
-                                            <p>申込日 : {{$ShootingPlans->application_date}}</p>
-                                        </div>
-                                        <div>
-                                            <p>撮影日 : {{$ShootingPlans->shooting_date}}</p>
-                                        </div>
-                                        <div>
-                                            <p>納品日 : {{$ShootingPlans->delivery_date}}</p>
-                                        </div>
-                                        <a href="{{ route('pilot_list.show',$pilot->id) }}" class="block text-center w-full py-3 mt-6 font-medium tracking-widest text-white uppercase bg-black shadow-lg focus:outline-none hover:bg-gray-900 hover:shadow-none">
-                                            申し込み
-                                        </a>
                                     </div>
                                 </div>
                             </div>
