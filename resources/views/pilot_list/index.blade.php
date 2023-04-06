7d9e031a9c791b774ba056f3117625e75ee29b4f
shouhei <shohei.nagamura@gmail.com>
2023-04-07 02:00:22 +0900

    作品一覧画面のCSSを修正

--- resources/views/pilot_list/index.blade.php
+++ resources/views/pilot_list/index.blade.php
@@ -13,9 +13,9 @@
 
     <div class="py-4">
         <div class="p-4 bg-white">
-            <div class="flex flex-wrap -mx-2" style="width:100%">
+            <div class="flex flex-wrap -mx-4">
                 @foreach ($portfolios as $portfolio)
-                <div class="w-full sm:w-1/2 md:w-1/2 lg:w-1/3">
+                <div class="w-full sm:w-1/2 md:w-1/2 lg:w-1/3 px-4 mb-8">
                     <div class="border border-grey-light rounded-lg overflow-hidden">
                         <iframe width="100%" height="225" src="{{$portfolio->portfolio_url}}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                         <div class="p-4 bg-gray-800">
