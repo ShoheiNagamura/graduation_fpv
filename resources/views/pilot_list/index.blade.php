5e42dc432f88026a89a4d731a7ae5878bf7b32c2
shouhei <shohei.nagamura@gmail.com>
2023-04-06 09:21:48 +0900

    CSSの修正

--- resources/views/pilot_list/index.blade.php
+++ resources/views/pilot_list/index.blade.php
@@ -12,33 +12,25 @@
     </x-slot>
 
     <div class="py-4">
-        <div class="max-w-7xl mx-auto">
-            <div class="overflow-hidden shadow-sm sm:rounded-sm">
-                <div class="p-2 bg-black border-b border-gray-200">
-                    <table class="text-center w-full border-collapse">
-                        @foreach ($pilots as $pilot)
-                        <tr class="hover:bg-grey-lighter">
-                            <td class="flex px-2 border-b border-grey-light">
-                                @foreach ($pilot->pilotPortfolios as $portfolio)
-                                <div class="p-2 flex-1">
-                                    <iframe width="420" height="225" src="{{$portfolio->portfolio_url}}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                                    <div>
-                                        <h3 class="text-left mt-4 font-bold text-lg text-gray-200">撮影者：{{$pilot->name}}({{$pilot->age}})</h3>
-                                    </div>
-                                    <div class="p-4">
-                                        <a href="{{ route('pilot.pilot_list.show',$pilot->id) }}" class="rounded-lg text-center py-2 px-8 font-medium text-black bg-gray-100 shadow-lg focus:outline-none hover:bg-gray-300 hover:shadow-none">
-                                            →
-                                        </a>
-                                    </div>
-                                </div>
-                                @endforeach
-                            </td>
-                        </tr>
-                        @endforeach
+        <div class="p-4 bg-black">
+            @foreach ($pilots as $pilot)
+            <div class="flex border-b border-grey-light">
+                @foreach ($pilot->pilotPortfolios as $portfolio)
+                <div class="p-2 flex-1">
+                    <iframe width="420" height="225" src="{{$portfolio->portfolio_url}}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    <div>
+                        <h3 class="text-left mt-4 font-bold text-lg text-gray-200">撮影者：{{$pilot->name}}({{$pilot->age}})</h3>
+                    </div>
+                    <div class="p-4">
+                        <a href="{{ route('pilot.pilot_list.show',$pilot->id) }}" class="rounded-lg text-center py-2 px-8 font-medium text-black bg-gray-100 shadow-lg focus:outline-none hover:bg-gray-300 hover:shadow-none">
+                            →
+                        </a>
+                    </div>
                 </div>
-                {{ $pilots->links() }}
-                </table>
+                @endforeach
             </div>
+            @endforeach
+            {{ $pilots->links() }}
         </div>
     </div>
 </x-pilot-layout>
