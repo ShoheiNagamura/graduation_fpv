6684cc3f937c6519e5852c7b29cece07374fbae5
shouhei <shohei.nagamura@gmail.com>
2023-04-08 17:10:37 +0900

    修正

--- database/migrations/2023_03_28_051051_create_orders_table.php
+++ database/migrations/2023_03_28_051051_create_orders_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->foreign('shooting_plan_id')->references('id')->on('shooting_plans')->onDelete('cascade');
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreignId('shooting_plan_id')->references('id')->on('shooting_plans')->onDelete('cascade');
+            $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->integer('status')->default(0);
             $table->datetime('application_date');
             $table->timestamps();
