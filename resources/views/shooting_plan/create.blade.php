07553d495475e1df0e196476d72834dc050cfd84
shouhei <shohei.nagamura@gmail.com>
2023-04-09 18:17:10 +0900

    CSS修正

--- resources/views/shooting_plan/create.blade.php
+++ resources/views/shooting_plan/create.blade.php
@@ -21,9 +21,9 @@
             <div class="">
                 <div class="bg-white">
                     @include('common.errors') <div class="text-center mb-6 uppercase font-bold text-lg text-grey-darkest">{{ __('プラン情報登録') }}</div>
-                    <form method="POST" action="{{ route('pilot.shooting_plan.store') }}" class="flex justify-center">
+                    <form method="POST" action="{{ route('pilot.shooting_plan.store') }}" class="flex justify-center mt-6">
                         @csrf
-                        <div class="flex flex-wrap w-2/4">
+                        <div class="flex flex-wrap w-2/4 mr-6">
                             <div class="w-full md:w-2/4 px-3 mb-6 md:mb-0">
                                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="plan_name" for="plan_name">{{ __('プラン名') }}</label>
                                 <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 @error('plan_name') is-invalid @enderror" id="plan_name" type="text" name="plan_name" value="{{ old('plan_name') }}" required autocomplete="plan_name" autofocus> <!-- //バリデーションエラー -->
@@ -41,8 +41,8 @@
                             </div>
                         </div>
 
-                        <div class="flex flex-wrap w-1/4">
-                            <div class="w-full md:w-1/4 px-3">
+                        <div class="w-1/4">
+                            <div class="w-full md:w-3/4">
                                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="plan_fee">{{ __('料金') }}</label>
                                 <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 @error('plan_fee') is-invalid @enderror" id="plan_fee" type="number" name="plan_fee" value="{{ old('plan_fee') }}" required autocomplete="plan_fee">
                                 <!-- バリデーションエラー -->
@@ -51,7 +51,7 @@
                                 @enderror
                             </div>
 
-                            <div class="w-full md:w-1/4 px-3 mb-6 md:mb-0">
+                            <div class="w-full md:w-3/4 mb-6 md:mb-0">
                                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="application_date">{{ __('申込日') }}</label>
                                 <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 @error('application_date') is-invalid @enderror" id="application_date" type="date" name="application_date" value="{{ old('application_date') }}" required>
                                 @error('application_date')
@@ -59,15 +59,15 @@
                                 @enderror
                             </div>
                         </div>
-                        <div class=" w-1/4">
-                            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                        <div class="w-1/4">
+                            <div class="w-full md:w-1/2 mb-6 md:mb-0">
                                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="shooting_date">{{ __('撮影実施日') }}</label>
                                 <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 @error('shooting_date') is-invalid @enderror" id="shooting_date" type="date" name="shooting_date" value="{{ old('shooting_date') }}" required>
                                 @error('shooting_date')
                                 <p class="text-red-500 text-xs italic">{{ $message }}</p>
                                 @enderror
                             </div>
-                            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                            <div class="w-full md:w-1/2 mb-6 md:mb-0">
                                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="delivery_date">{{ __('納品日') }}</label>
                                 <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 @error('delivery_date') is-invalid @enderror" id="delivery_date" type="date" name="delivery_date" value="{{ old('delivery_date') }}" required>
                                 @error('delivery_date')
