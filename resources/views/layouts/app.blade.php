8e6dcac6b88423d2549309479fd19b8772d614d7
shouhei <shohei.nagamura@gmail.com>
2023-04-17 19:06:53 +0900

    発注画面作成

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
 </head>
 
 <body class="font-sans antialiased">
-    <div class="min-h-screen bg-black">
+    <div class="min-h-screen bg-white">
         @include('layouts.navigation')
 
         <!-- Page Heading -->
--- resources/views/user_dashboard.blade.php
+++ resources/views/user_dashboard.blade.php
@@ -1,18 +1,34 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('管理画面') }}
+            {{ __('依頼画面') }}
         </h2>
     </x-slot>
 
-    <div class="py-12 bg-white">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                    {{ __("一般ユーザー") }}
-                </div>
+
+    <div class="flex justify-center bg-white p-6">
+        <div class="w-1/4 flex justify-center">
+            <div class="flex flex-col justify-start">
+                <a href="{{ route('profile.edit')}}" class="py-2 px-4 text-gray-800 hover:bg-gray-100">基本プロフィール</a>
+                <a href="{{route('user_dashboard')}}" class="py-2 px-4 text-gray-800 hover:bg-gray-100">ご依頼案件管理</a>
+                <a href="#" class="py-2 px-4 text-gray-800 hover:bg-gray-100">報酬管理</a>
+                <a href="#" class="py-2 px-4 text-gray-800 hover:bg-gray-100">メッセージ</a>
             </div>
         </div>
+        <div class="w-3/4">
+            <table class="w-full border-collapse border border-gray-200">
+                <thead>
+                    <tr class="bg-gray-100 text-left">
+                        <th class="text-center py-2 px-4">ご依頼案件</th>
+                        <th class="text-center py-2 px-4">担当操縦士</th>
+                        <th class="text-center py-2 px-4">ご依頼日</th>
+                        <th class="text-center py-2 px-4">ステータス</th>
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
+        </div>
     </div>
+
 </x-app-layout>
